69c3da2d22b60e1071c4093a59292951b6af7634
NiccoleP <82088136+NiccoleP@users.noreply.github.com>
2021-04-19 15:07:37 +0200

    Update Poly_Sim.R

--- Poly_Sim.R
+++ Poly_Sim.R
@@ -11,7 +11,6 @@ args = commandArgs(trailingOnly=TRUE)
 height <- read_tsv(args[1])#"50_irnt.gwas.imputed_v3.both_sexes.tsv"
 blocks <- read_tsv(args[2],col_names=c("chr","start","end"))#"fourier_ls-chr3.bed"
 variants <- read_tsv(args[3])#"variants.tsv"
-
 #subset chr 3 
 height_chr3 <- filter(height, str_detect(variant, "^3:"))
 variants_chr3 <- filter(variants, str_detect(variant, "^3:"))
@@ -38,39 +37,41 @@ height_chr3 <- inner_join(height_chr3, regions, by = c("bed"="index"))
 # get the best p-value per ld_block 
 independentA <- height_chr3 %>% 
   group_by(ld_block) %>% 
-  slice_min(pval, n = 1)
+  slice_min(pval, n = 1) 
 
 independentB <- height_chr3 %>% 
   filter(pval <= 5e-8) %>%
   group_by(ld_block) %>% 
   slice_min(pval, n = 1)
 
-# subset only those variants that have rsID
-independent_shortA<-inner_join(variants_chr3, independentA, by = c("pos"="pos"))
-independent_shortB<-inner_join(variants_chr3, independentB, by = c("pos"="pos"))
-
 # get columns of interest from the variants file  and filter those that have proper rsID 
-independent_shortA<-inner_join(variants_chr3, independentA, by = c("pos"="pos")) %>%
+independentA<-inner_join(variants_chr3, independentA, by = c("pos"="pos")) %>%
                     filter(str_detect(rsid, "^rs"))
-independent_shortB<-inner_join(variants_chr3, independentB, by = c("pos"="pos")) %>%
+independentB<-inner_join(variants_chr3, independentB, by = c("pos"="pos")) %>%
                     filter(str_detect(rsid, "^rs"))
 
 # retrieve ancestral allele
 database_url<-"http://grch37.rest.ensembl.org/variation/human/"
-ancestral_alleleA<-c()
+ancestral_alleleA<-c();ancestral_alleleB<-c()
+
 summary_data<-data.frame()
-for( i in 1:length(independent_shortA$rsid)){
+for( i in 1:length(independentA$rsid)){
   message("Retrieving id \t", i)
-  summary_data<-fromJSON(paste0(database_url,independent_shortA$rsid[i]))
+  summary_data<-fromJSON(paste0(database_url,independentA$rsid[i]))
   ancestral_alleleA[i]<-summary_data$mappings$ancestral_allele
 }
 
 # polarize by ancestral allele 
-for(i in 1:length(independent_shortA$rsid)){
+for(i in 1:length(independentA$rsid)){
   if(is.na(ancestral_alleleA[i])){
-    ancestral_alleleA[i]<-independent_shortA$major[i]
+    ancestral_alleleA[i]<-independentA$major[i]
   }
-  if(isTRUE(independent_shortA$major[i] != ancestral_alleleA[i])){ # change sign 
-    independent_shortA$beta[i]<-(-1)*independent_shortA$beta[i]
+  if(isTRUE(independentA$major[i] != ancestral_alleleA[i])){ # change sign 
+    independentA$beta[i]<-(-1)*independentA$beta[i]
   }
 }
+
+
+summary_stats<-c(mean(na.rm = TRUE,height_chr3$beta),sd(height_chr3$beta,na.rm = TRUE))
+write_csv(x = summary_stats,file = "summary_stats")
+writeCsv(x=independentA,file="independent_variants")
